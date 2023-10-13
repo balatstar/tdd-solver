52a2fb8b8ebccced674df9bb8dfe70163e6ba9fc
MargaretTechworld <MargaretTechworld@gmail.com>
2023-10-13 08:28:02 +0000

    Add test for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,4 +8,19 @@ describe Solver do
   it 'returns a new solver object' do
     expect(@solver).to be_an_instance_of Solver
   end
+  describe 'factorial' do
+    it 'returns an error for n<0' do
+      expect { @solver.factorial(-3) }.to raise_error(StandardError, "Negative integers not allowed.")
+    end
+
+    it 'returns the correct factorial for 0' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the correct factorial for n>1' do
+      expect(@solver.factorial(5)).to eq(120)
+    end
+  end
+
+
 end
