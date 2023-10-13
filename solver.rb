ed82f98b04065d108b1123f343bc24c88dc74794
MargaretTechworld <margarettechworld@gmail.com>
2023-10-13 08:38:43 +0000

    Define method to slove factorial

--- solver.rb
+++ solver.rb
@@ -1,3 +1,15 @@
 class Solver
-
+    def factorial(n)
+        if n < 0
+          raise StandardError, "Negative integers not allowed."
+        elsif n == 0
+          return 1
+        else
+          result = 1
+          (1..n).each do |i|
+            result *= i
+          end
+          return result
+        end
+      end    
 end
