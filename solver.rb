6a1349836fa2f984b40dbf6d9c5123ea0f72e045
joyce <joyce.ann.cruz@gmail.com>
2023-10-13 16:42:31 +0800

    Add method for reverse

--- solver.rb
+++ solver.rb
@@ -1,15 +1,19 @@
 class Solver
-    def factorial(n)
-        if n < 0
-          raise StandardError, "Negative integers not allowed."
-        elsif n == 0
-          return 1
-        else
-          result = 1
-          (1..n).each do |i|
-            result *= i
-          end
-          return result
-        end
-      end    
+  def factorial(n)
+    if n < 0
+      raise StandardError, "Negative integers not allowed."
+    elsif n == 0
+      return 1
+    else
+      result = 1
+      (1..n).each do |i|
+        result *= i
+      end
+      return result
+    end
+  end
+
+  def reverse(word)
+    word.reverse
+  end
 end
